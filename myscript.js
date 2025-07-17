463a04590d3d0ca9228d44795b56739724b59f1f
edmond-bobiles <edmondjohn.bobiles@gmail.com>
2025-07-17 15:28:03 +0800

    Created input for human choice and generates random computer choice for rock, paper, or scissors

--- myscript.js
+++ myscript.js
@@ -1,4 +1,35 @@
-    console.log("Hello from JavaScript!");
-    function greet(name) {
-        return "Hello, " + name + "!";
-    }
+function getRandomInt(){
+    return Math.floor(Math.random() * 3)
+}
+
+function getComputerChoice(){
+    let generate = getRandomInt();
+
+    if (generate == 0)
+        return "rock";
+    else if (generate == 1)
+        return "paper";
+    else
+        return "scissors";
+}
+
+let humanChoice;
+
+do {
+  humanChoice = prompt("Enter rock, paper, or scissors").toUpperCase();
+} while (humanChoice !== "ROCK" && humanChoice !== "PAPER" && humanChoice !== "SCISSORS");
+
+
+// getComputerChoice that randomly returns rock, paper, or scissors
+// Use Math.random to generate between 0, 1, and 2
+// Convert the number to rock, paper, or scissors
+
+// getHumanChoice via input, case-insensitive
+
+// playRound checks if computer or human wins and allot score to humanScore or computerScore
+// playRound console logs the output
+// increment the score
+
+// 5 rounds
+// playGame calls playRound 5 times
+// declare winner at the end
